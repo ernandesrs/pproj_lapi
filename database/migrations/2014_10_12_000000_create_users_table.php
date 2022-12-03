261ae155e7c26d702618f0abb123da0e56e8e1c3
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-12-03 14:23:13 -0300

    Updating user model, migrate and factory

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('username')->unique();
+            $table->string('gender', 1)->default('n');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('verification_token', 50)->nullable()->default(null);
+            $table->string('photo')->nullable()->default(null);
+            $table->integer('level')->default(0);
+
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
