2cdb16dc29d08997bebdf900bacac9fdd6a77a42
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-12-03 21:41:18 -0300

    Updating password(via reset link)

--- app/Http/Controllers/Auth/AccountController.php
+++ app/Http/Controllers/Auth/AccountController.php
@@ -8,12 +8,14 @@ use App\Exceptions\Account\LoginFailException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Account\ForgetRequest;
 use App\Http\Requests\Account\LoginRequest;
+use App\Http\Requests\Account\UpdatePasswordRequest;
 use App\Http\Requests\Request\AccountRequest;
 use App\Models\PasswordReset;
 use App\Models\User;
 use App\Services\Account\AccountService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class AccountController extends Controller
@@ -86,9 +88,27 @@ class AccountController extends Controller
         ]);
     }
 
-    public function updatePassword(Request $request)
+    /**
+     * Update password
+     *
+     * @param UpdatePasswordRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function updatePassword(UpdatePasswordRequest $request)
     {
-        dd($request->all());
+        $validated = $request->validated();
+
+        $resetPassword = PasswordReset::where("token", $validated["token"])->firstOrFail();
+        $user = User::where("email", $resetPassword->email)->firstOrFail();
+        $user->update([
+            "password" => Hash::make($validated["password"])
+        ]);
+
+        PasswordReset::where("email", $user->email)->delete();
+
+        return response()->json([
+            "success" => true
+        ]);
     }
 
     /**
