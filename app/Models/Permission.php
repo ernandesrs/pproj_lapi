950f9c6cc81f76238aeff752a4ce14a3e9bee9c9
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-12-11 11:08:31 -0300

    Permission model,migration and factory

---
+++ app/Models/Permission.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    use HasFactory;
+
+    public const RULABLES = [
+        'permission' => Permission::class,
+        'user' => User::class
+    ];
+
+    public const RULES = ['show', 'create', 'update', 'delete', 'force_delete', 'recovery'];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(50)->create();
+        \App\Models\Permission::factory(5)->create();
     }
 }
