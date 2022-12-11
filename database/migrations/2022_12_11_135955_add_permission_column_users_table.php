f9231725209256bec367431ef64b8f4ffe7ec376
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-12-11 18:48:36 -0300

    Add permission column on user table

---
+++ database/migrations/2022_12_11_135955_add_permission_column_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPermissionColumnUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('permission_id')->nullable();
+
+            $table->foreign('permission_id')->references('id')->on('permissions')->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
