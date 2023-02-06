b9a2289b03c91ae98902296e7fe6a35f1631d50a
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2023-02-06 15:58:54 -0400

    Create, list and show subscription

--- app/Http/Controllers/Dash/SubscriptionController.php
+++ app/Http/Controllers/Dash/SubscriptionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Dash;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SubscriptionRequest;
+use App\Models\Subscription;
 use Illuminate\Http\Request;
 
 class SubscriptionController extends Controller
@@ -11,33 +12,65 @@ class SubscriptionController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
-        //
+        return response()->json([
+            "success" => true,
+            "subscriptions" => \Auth::user()->subscriptions()->get()
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  SubscriptionRequest  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(SubscriptionRequest $request)
     {
-        dd($request->validated());
+        $period = $request->get("period", 1);
+        $installments = $request->get("installments", 1);
+
+        $creditCard = $request->user()->creditCards()->where("id", $request->get("card_id"))->first();
+        if (!$creditCard) {
+            return response()->json([
+                "success" => false,
+                "message" => "Credit card not found.",
+            ]);
+        }
+
+        // faça uma cobrança no cartão
+        // ...
+
+        $subscription = \Auth::user()->subscriptions()->create([
+            "starts_in" => now(),
+            "ends_in" => now()->addMonth($period),
+            "type" => Subscription::TYPE_NEW,
+            "status" => Subscription::STATUS_ACTIVE
+        ]);
+
+        return response()->json([
+            "success" => true,
+            "subscription" => $subscription
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show($id)
     {
-        //
+        $subscription = \Auth::user()->subscriptions()->where("id", $id)->first();
+
+        return response()->json([
+            "success" => true,
+            "subscription" => $subscription
+        ]);
     }
 
     /**
@@ -45,21 +78,21 @@ class SubscriptionController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, $id)
     {
-        //
+        return response()->json([]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($id)
     {
-        //
+        return response()->json([]);
     }
 }
