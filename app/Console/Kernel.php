d3520903694d6ff42326c45f3a8076d7053702fe
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-08-26 10:27:15 -0400

    [All] Task schedule to delete unverified users

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Services\UserService;
+use Illuminate\Support\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -16,6 +18,23 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        /**
+         * Delete unverified users
+         */
+        if (is_numeric(env('DAYS_TO_DELETE_UNVERIFIED_USER'))) {
+            /**
+             * Finds and deletes unverified users who missed set verification timeout
+             */
+            $schedule->call(function () {
+                \App\Models\User::whereNull("email_verified_at")
+                    ->where("created_at", "<", Carbon::now()->subDays(env('DAYS_TO_DELETE_UNVERIFIED_USER')))
+                    ->get()
+                    ->each(function ($user) {
+                        (new UserService())->delete($user);
+                    });
+            })->daily();
+        }
     }
 
     /**
@@ -25,7 +44,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
